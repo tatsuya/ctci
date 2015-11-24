be9e3840b9d118c19be0801e60151eb45a3c7d79
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2015-11-24 15:59:18 +0900

    9-1

--- java/src/main/java/com/tatsuyaoiw/chapter9/Question1.java
+++ java/src/main/java/com/tatsuyaoiw/chapter9/Question1.java
@@ -3,16 +3,34 @@ package com.tatsuyaoiw.chapter9;
 public class Question1 {
 
 	void merge(int[] a, int[] b) {
-		// TODO
+		for (int bi = 0; bi < b.length; bi++) {
+			int newIndex = findIndex(a, b[bi]);
+			for (int ai = getLength(a); ai > newIndex; ai--) {
+				a[ai] = a[ai-1];
+			}
+			a[newIndex] = b[bi];
+		}
 	}
 
 	int findIndex(int[] a, int n) {
-		for (int i = 0; i < a.length; i++) {
+		int len = getLength(a);
+		for (int i = 0; i < len; i++) {
 			int e = a[i];
 			if (n <= e) {
 				return i;
 			}
 		}
-		return a.length;
+		return len;
+	}
+
+	int getLength(int[] a) {
+		int len = 0;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] == 0) {
+				break;
+			}
+			len++;
+		}
+		return len;
 	}
 }
--- java/src/test/java/com/tatsuyaoiw/chapter9/Question1Test.java
+++ java/src/test/java/com/tatsuyaoiw/chapter9/Question1Test.java
@@ -3,6 +3,8 @@ package com.tatsuyaoiw.chapter9;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class Question1Test {
@@ -14,9 +16,29 @@ public class Question1Test {
 	}
 
 	@Test
+	public void testMerge() throws Exception {
+		int[] a = new int[20];
+		for (int i = 0; i < 10; i++) {
+			a[i] = i+1;
+		}
+
+		int[] b = {3, 6, 9, 12};
+		q.merge(a, b);
+		assertEquals("[1, 2, 3, 3, 4, 5, 6, 6, 7, 8, 9, 9, 10, 12, 0, 0, 0, 0, 0, 0]", Arrays.toString(a));
+	}
+
+	@Test
 	public void testFindIndex() throws Exception {
-		int[] a1 = {1, 2, 3};
-		int index = q.findIndex(a1, 1);
-		assertEquals(0, index);
+		int[] a = {1, 2, 3};
+		int ai = q.findIndex(a, 1);
+		assertEquals(0, ai);
+
+		int[] b = {1, 3, 5};
+		int bi = q.findIndex(b, 4);
+		assertEquals(2, bi);
+
+		int[] c = {1, 2, 3};
+		int ci = q.findIndex(c, 4);
+		assertEquals(3, ci);
 	}
 }
